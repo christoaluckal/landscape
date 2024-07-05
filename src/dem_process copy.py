6193ddbcac6fc2b913ed4154ba1c11963ce24a03
Christo Aluckal <christo12aluckal@gmail.com>
2024-07-05 19:48:37 -0400

    added bidirectional geodetic to local ENU

--- src/dem_process copy.py
+++ src/dem_process copy.py
@@ -5,6 +5,7 @@ import pyproj
 import pyproj.transformer
 from tqdm import tqdm
 from scipy.spatial import KDTree
+import math
 
 
 def pixel2latlon(x, y,gt_0, gt_1, gt_2, gt_3, gt_4, gt_5):
@@ -47,6 +48,50 @@ def geodedic_to_ecef( lati, longi, alti ):
 
     return X,Y,Z
 
+def xyz2llh(x,y,z):
+    '''
+    https://gis.stackexchange.com/a/292635
+    Function to convert xyz ECEF to llh
+    convert cartesian coordinate into geographic coordinate
+    ellipsoid definition: WGS84
+      a= 6,378,137m
+      f= 1/298.257
+
+    Input
+      x: coordinate X meters
+      y: coordinate y meters
+      z: coordinate z meters
+    Output
+      lat: latitude rad
+      lon: longitude rad
+      h: height meters
+    '''
+    # --- WGS84 constants
+    a = 6378137.0
+    f = 1.0 / 298.257223563
+    # --- derived constants
+    b = a - f*a
+    e = math.sqrt(math.pow(a,2.0)-math.pow(b,2.0))/a
+    clambda = math.atan2(y,x)
+    p = math.sqrt(pow(x,2.0)+pow(y,2))
+    h_old = 0.0
+    # first guess with h=0 meters
+    theta = math.atan2(z,p*(1.0-math.pow(e,2.0)))
+    cs = math.cos(theta)
+    sn = math.sin(theta)
+    N = math.pow(a,2.0)/math.sqrt(math.pow(a*cs,2.0)+math.pow(b*sn,2.0))
+    h = p/cs - N
+    while abs(h-h_old) > 1.0e-6:
+        h_old = h
+        theta = math.atan2(z,p*(1.0-math.pow(e,2.0)*N/(N+h)))
+        cs = math.cos(theta)
+        sn = math.sin(theta)
+        N = math.pow(a,2.0)/math.sqrt(math.pow(a*cs,2.0)+math.pow(b*sn,2.0))
+        h = p/cs - N
+    # llh = {'lon':clambda, 'lat':theta, 'height': h}
+    llh = {'lon':np.degrees(clambda), 'lat':np.degrees(theta), 'height': h}
+    return llh
+
 def compute_ecef_to_enu_transform( lati_r, longi_r ):
     """ Computes a matrix_3x3 which transforms a ecef diff-point to ENU (East-North-Up)
         Needs as input the latitude and longitude (in degrees) of the reference point
@@ -76,6 +121,17 @@ def compute_ecef_to_enu_transform( lati_r, longi_r ):
 
     T_enu_ecef = T
     return T_enu_ecef
+
+def ecef2enu( points, lati_r, longi_r, ref_point ):
+    T = compute_ecef_to_enu_transform( lati_r, longi_r )
+    T = np.linalg.inv(T)
+    p = np.dot(T, points.T).T
+    if type(ref_point) != np.ndarray:
+        ref_point = np.array(ref_point)
+
+    p = p + ref_point
+
+    return p
 
 
 
@@ -177,8 +233,8 @@ ele_masked[mask] = min
 ele = np.copy(ele_masked)
 # ele_norm = (ele_masked - min) / (max - min)
 
-# plt.imshow(ele_masked)
-# plt.show()
+plt.matshow(ele_masked)
+plt.show()
 
 gt = dataset.GetGeoTransform()
 
@@ -202,59 +258,119 @@ latlon_extent = []
 for i in im_extent:
     latlon_extent.append(i)
 
+NW_lat, NW_lon = pixel2latlon(latlon_extent[0][0], latlon_extent[0][1], *gt)
+C_lat, C_lon = pixel2latlon(latlon_extent[4][0], latlon_extent[4][1], *gt)
+SE_lat, SE_lon = pixel2latlon(latlon_extent[8][0], latlon_extent[8][1], *gt)
+C_interp_lat, C_interp_lon = (NW_lat + SE_lat) / 2, (NW_lon + SE_lon) / 2
 
-ex1_lat, ex1_lon = pixel2latlon(latlon_extent[0][0], latlon_extent[0][1], *gt)
-ex3_lat, ex3_lon = pixel2latlon(latlon_extent[2][0], latlon_extent[2][1], *gt)
-ex7_lat, ex7_lon = pixel2latlon(latlon_extent[6][0], latlon_extent[6][1], *gt)
-ex9_lat, ex9_lon = pixel2latlon(latlon_extent[8][0], latlon_extent[8][1], *gt)
-center_lat, center_lon = pixel2latlon(latlon_extent[4][0], latlon_extent[4][1], *gt)
-print(f"Center LatLon: {center_lat}, {center_lon}, {ele[im_extent[4][0], im_extent[4][1]]}")
+print(f"NW: {NW_lat}, {NW_lon}, {ele[im_extent[0][0], im_extent[0][1]]}")
+print(f"C: {C_lat}, {C_lon}, {ele[im_extent[4][0], im_extent[4][1]]}")
+print(f"C_i: {C_interp_lat}, {C_interp_lon}")
+print(f"C Error: {C_lat-C_interp_lat}, {C_lon-C_interp_lon}")
+print(f"SE: {SE_lat}, {SE_lon}, {ele[im_extent[8][0], im_extent[8][1]]}")
 
-res_x = gt[1]
-res_y = gt[5]
+print(f"{NW_lat-SE_lat}, {NW_lon-SE_lon}")
 
-X = []
-Y = []
-Z = []
+NW_X, NW_Y, NW_Z = geodedic_to_ecef(NW_lat, NW_lon, ele[im_extent[0][0], im_extent[0][1]])
+C_X, C_Y, C_Z = geodedic_to_ecef(C_lat, C_lon, ele[im_extent[4][0], im_extent[4][1]])
+SE_X, SE_Y, SE_Z = geodedic_to_ecef(SE_lat, SE_lon, ele[im_extent[8][0], im_extent[8][1]])
 
-Xr,Yr,Zr = geodedic_to_ecef(center_lat,center_lon,ele[im_extent[4][0], im_extent[4][1]])
-T = compute_ecef_to_enu_transform(ex7_lat,ex7_lon)
+T = compute_ecef_to_enu_transform(C_lat, C_lon)
 
-for i in tqdm(range(0,ele_masked.shape[0],10)):
-    for j in range(0,ele_masked.shape[1],10):
-        lat,lon = pixel2latlon(j,i,*gt)
-        alt = ele_masked[i,j]
-        
-        Xp,Yp,Zp = geodedic_to_ecef(lat,lon,alt)
-        X.append(Xp)
-        Y.append(Yp)
-        Z.append(Zp)
-
+X = [NW_X, C_X, SE_X]
+Y = [NW_Y, C_Y, SE_Y]
+Z = [NW_Z, C_Z, SE_Z]
 
 X = np.array(X)
 Y = np.array(Y)
 Z = np.array(Z)
 
-delta = np.array([X-Xr,Y-Yr,Z-Zr])
+delta = np.array([X-C_X,Y-C_Y,Z-C_Z])
 p = np.dot(T,delta).T
 
-point_tree = KDTree(p)
+NW_x_local, NW_y_local, NW_z_local = p[0]
+C_x_local, C_y_local, C_z_local = p[1]
+SE_x_local, SE_y_local, SE_z_local = p[2]
 
-computeOrientation(point_tree)
-# p = rotate(p,[90,0,0])
+# ENU to ECEF
+NW_ecef = ecef2enu(np.array([NW_x_local, NW_y_local, NW_z_local]), C_lat, C_lon, [C_X, C_Y, C_Z])
+C_ecef = ecef2enu(np.array([C_x_local, C_y_local, C_z_local]), C_lat, C_lon, [C_X, C_Y, C_Z])
+SE_ecef = ecef2enu(np.array([SE_x_local, SE_y_local, SE_z_local]), C_lat, C_lon, [C_X, C_Y, C_Z])
 
-# reflect about Y axis
-# p[:,2] = -p[:,2]
+print(f"NW ECEF: {NW_ecef}")
+print(f"C ECEF: {C_ecef}")
+print(f"SE ECEF: {SE_ecef}")
 
-import pickle
+# ECEF to LLH
+NW_llh = xyz2llh(*NW_ecef)
+C_llh = xyz2llh(*C_ecef)
+SE_llh = xyz2llh(*SE_ecef)
 
-with open('workspace.pkl', 'wb') as f:
-    pickle.dump(p, f)
+print(f"NW LLH: {NW_llh}")
+print(f"C LLH: {C_llh}")
+print(f"SE LLH: {SE_llh}")
 
-import open3d as o3d
+test_X, test_Y = 10, 10
+test_Z = C_z_local
 
-pcd = o3d.geometry.PointCloud()
-pcd.points = o3d.utility.Vector3dVector(p)
+test_ecef = ecef2enu(np.array([test_X, test_Y, test_Z]), C_lat, C_lon, [C_X, C_Y, C_Z])
+test_llh = xyz2llh(*test_ecef)
 
-o3d.visualization.draw_geometries([pcd])
-o3d.io.write_point_cloud("space_2.ply", pcd)
+print(f"Test LLH: {test_llh}")
+
+
+# ex1_lat, ex1_lon = pixel2latlon(latlon_extent[0][0], latlon_extent[0][1], *gt)
+# ex3_lat, ex3_lon = pixel2latlon(latlon_extent[2][0], latlon_extent[2][1], *gt)
+# ex7_lat, ex7_lon = pixel2latlon(latlon_extent[6][0], latlon_extent[6][1], *gt)
+# ex9_lat, ex9_lon = pixel2latlon(latlon_extent[8][0], latlon_extent[8][1], *gt)
+# center_lat, center_lon = pixel2latlon(latlon_extent[4][0], latlon_extent[4][1], *gt)
+# print(f"Center LatLon: {center_lat}, {center_lon}, {ele[im_extent[4][0], im_extent[4][1]]}")
+
+# res_x = gt[1]
+# res_y = gt[5]
+
+# X = []
+# Y = []
+# Z = []
+
+# Xr,Yr,Zr = geodedic_to_ecef(center_lat,center_lon,ele[im_extent[4][0], im_extent[4][1]])
+# T = compute_ecef_to_enu_transform(ex7_lat,ex7_lon)
+
+# for i in tqdm(range(0,ele_masked.shape[0],10)):
+#     for j in range(0,ele_masked.shape[1],10):
+#         lat,lon = pixel2latlon(j,i,*gt)
+#         alt = ele_masked[i,j]
+        
+#         Xp,Yp,Zp = geodedic_to_ecef(lat,lon,alt)
+#         X.append(Xp)
+#         Y.append(Yp)
+#         Z.append(Zp)
+
+
+# X = np.array(X)
+# Y = np.array(Y)
+# Z = np.array(Z)
+
+# delta = np.array([X-Xr,Y-Yr,Z-Zr])
+# p = np.dot(T,delta).T
+
+# point_tree = KDTree(p)
+
+# computeOrientation(point_tree)
+# # p = rotate(p,[90,0,0])
+
+# # reflect about Y axis
+# # p[:,2] = -p[:,2]
+
+# import pickle
+
+# with open('workspace.pkl', 'wb') as f:
+#     pickle.dump(p, f)
+
+# import open3d as o3d
+
+# pcd = o3d.geometry.PointCloud()
+# pcd.points = o3d.utility.Vector3dVector(p)
+
+# o3d.visualization.draw_geometries([pcd])
+# o3d.io.write_point_cloud("space_2.ply", pcd)
